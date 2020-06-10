797144c7e834fdc3463363edf1e13c96b5c5b4ed
Akhil Narang <me@akhilnarang.dev>
2020-06-10 10:45:13 +0530

    gauguin: import keylayout
      * Disable DPAD_HOME
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- device.mk
+++ device.mk
@@ -46,6 +46,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
