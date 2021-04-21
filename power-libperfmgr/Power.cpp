PixelExperience-Devices/device_xiaomi_cepheus

cepheus: power-libperfmgr: Add support for event-based tap to wake gesture control
Change-Id: Id963e3c7302a7ee46a5a6b6d1b743a5718d71f4c
Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

power-libperfmgr/Power.cpp
@@ -30,6 +30,12 @@
 
 #include "Power.h"
 
+#include <linux/input.h>
+
+constexpr char kWakeupEventNode[] = "/dev/input/event4";
+constexpr int kWakeupModeOff = 4;
+constexpr int kWakeupModeOn = 5;
+
 namespace android {
 namespace hardware {
 namespace power {
@@ -191,8 +197,20 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     return Void();
 }
 
-Return<void> Power::setFeature(Feature /*feature*/, bool /*activate*/) {
-    // Nothing to do
+Return<void> Power::setFeature(Feature feature, bool activate) {
+    switch (feature) {
+        case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
+            int fd = open(kWakeupEventNode, O_RDWR);
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = activate ? kWakeupModeOn : kWakeupModeOff;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+            } break;
+        default:
+            break;
+    }
     return Void();
 }
 

DtHnAme/device_xiaomi_gauguin

power-libperfmgr/Power.cpp
@@ -30,6 +30,12 @@
 
 #include "Power.h"
 
+#include <linux/input.h>
+
+constexpr char kWakeupEventNode[] = "/dev/input/event2";
+constexpr int kWakeupModeOff = 4;
+constexpr int kWakeupModeOn = 5;
+
 namespace android {
 namespace hardware {
 namespace power {
@@ -191,8 +197,20 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     return Void();
 }
 
-Return<void> Power::setFeature(Feature /*feature*/, bool /*activate*/) {
-    // Nothing to do
+Return<void> Power::setFeature(Feature feature, bool activate) {
+    switch (feature) {
+        case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
+            int fd = open(kWakeupEventNode, O_RDWR);
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = activate ? kWakeupModeOn : kWakeupModeOff;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+            } break;
+        default:
+            break;
+    }
     return Void();
 }
 