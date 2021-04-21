bc6d8412d094849f55fc4b8e7093ce0395b80182
DtHnAme <1904685725@qq.com>
2021-04-21 21:30:33 +0800

    gauguin: power-libperfmgr: Modify for system

--- power-libperfmgr/hidl/Power.cpp
+++ power-libperfmgr/hidl/Power.cpp
@@ -50,7 +50,7 @@ constexpr char kPowerHalStateProp[] = "vendor.powerhal.state";
 constexpr char kPowerHalAudioProp[] = "vendor.powerhal.audio";
 constexpr char kPowerHalInitProp[] = "vendor.powerhal.init";
 constexpr char kPowerHalRenderingProp[] = "vendor.powerhal.rendering";
-constexpr char kPowerHalConfigPath[] = "/vendor/etc/powerhint.json";
+constexpr char kPowerHalConfigPath[] = "/system/etc/powerhint.json";
 
 Power::Power()
     : mHintManager(nullptr),
