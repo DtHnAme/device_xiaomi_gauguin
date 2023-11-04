bdca9b00249d0c3328544e4a5b1bed4c513c705f
DtHnAme <1904685725@qq.com>
2023-11-04 10:14:40 +0800

    Revert "gauguin: Build mtdservice interface lib from source"
    This reverts commit 72d19be466f1a3d229537e7646b75eff6ae99d6c.

--- device.mk
+++ device.mk
@@ -307,8 +307,7 @@ PRODUCT_COPY_FILES += \
 
 # Mlipay
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
 
 # Native Public Libraries
 PRODUCT_COPY_FILES += \
